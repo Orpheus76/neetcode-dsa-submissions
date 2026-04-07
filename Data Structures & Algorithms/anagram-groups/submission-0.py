9329962cf0e8d42f300b2e0f972fb531f9861bb0
Lambert Kylian <143292197+Orpheus76@users.noreply.github.com>
2026-04-07 12:23:16 +0200

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        wordMap = collections.defaultdict(list)
+        res = []
+        sub_res = []
+        
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+
+            wordMap[sorted_word].append(word)
+        
+        return list(wordMap.values())
